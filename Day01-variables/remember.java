d61db0142c8ffa4704d74d954687a7733c4c5930
VaineO <abnerviniciusa@gmail.com>
2026-06-03 23:21:44 -0400

    created the new file 'remember'

---
+++ Day01-variables/remember.java
@@ -0,0 +1,13 @@
+// remember this structure of Java
+
+public class remember {
+
+    public static void main(String[] args) {
+
+        // create your code in Java
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+}
