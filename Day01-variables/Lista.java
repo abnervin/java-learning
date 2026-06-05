5ee1a16b4dbdfefbdf6998bb7588cab5b5978e85
VaineO <abnerviniciusa@gmail.com>
2026-06-04 21:09:32 -0400

    brincando de codarkkk

---
+++ Day01-variables/Lista.java
@@ -0,0 +1,14 @@
+public class Lista{
+    public static void main(String[] args) {
+
+        int idd = 26;
+        String nm = "Abner Vinicius";
+        double alt = 1.80;
+        boolean joysoyLOKO = true;
+
+        System.out.println(idd);
+        System.out.println(nm);
+        System.out.println(alt);
+        System.out.println(joysoyLOKO);
+    }
+}
