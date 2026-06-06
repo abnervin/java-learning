ee1acfede4f7add9eda362e3b69a235539495e45
VaineO <abnerviniciusa@gmail.com>
2026-06-06 13:46:17 -0400

    novo dia 1

--- Day01-variables/Lista.java
+++ Day01-variables/Lista.java
@@ -1,14 +1,15 @@
-public class Lista{
+public class Lista {
+
     public static void main(String[] args) {
+        
+        int idd = 25;
+        String nome = "abner";
+        int soma = idd+24*5;
 
-        int idd = 26;
-        String nm = "Abner Vinicius";
-        double alt = 1.80;
-        boolean joysoyLOKO = true;
-
+        System.out.println("\n===============");
         System.out.println(idd);
-        System.out.println(nm);
-        System.out.println(alt);
-        System.out.println(joysoyLOKO);
+        System.out.println(nome);
+        System.err.println("Voce consegue " + soma);
+        System.out.println("\n===============");
     }
 }
