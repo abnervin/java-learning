9bf695f30c34746a1cd90e4c1f3562d643785f52
VaineO <abnerviniciusa@gmail.com>
2026-06-12 22:53:48 -0400

    Codigos re feitos e organizados

--- Day03-condition/login.java
+++ Day03-condition/login.java
@@ -1,41 +1,38 @@
 import java.util.Scanner;
 
 public class login {
-
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("\n================================");
+        System.out.println("=========SISTEMA DE LOGIN=========");
+        System.out.println("\n================================");
+        System.out.println("Digite Seu nome de usuario !? ");
+        String user = sc.nextLine();
+        
+        if (user.equals ("Invictus")) {
 
-        Scanner sc = new Scanner(System.in);
+            System.out.println("Usuario Encontrado!!");
+            System.out.println("Digite Sua senha !? ");
+            String pass = sc.nextLine();
+            if (pass.equals ("21654")) {
 
-        final String USUARIO_CORRETO = "Invictus";
-        final String SENHA_CORRETA = "abner";
-
-        System.out.println("=================================");
-        System.out.println("      SISTEMA DE LOGIN");
-        System.out.println("=================================");
-
-        System.out.print("Usuário: ");
-        String usuario = sc.nextLine();
-
-        if (usuario.equals(USUARIO_CORRETO)) {
-
-            System.out.println("\n✅ Usuário encontrado!");
-
-            System.out.print("Senha: ");
-            String senha = sc.nextLine();
-
-            if (senha.equals(SENHA_CORRETA)) {
-                System.out.println("\n🔓 Acesso liberado!");
-                System.out.println("Bem-vindo, " + usuario + "!");
-            } else {
-                System.out.println("\n❌ Senha incorreta!");
-                System.out.println("Acesso negado.");
+                System.out.println("acesso Liberado!!");
+                
+            }
+            else{
+                System.out.println("Senha Incorreta!!!!");
             }
 
-        } else {
-            System.out.println("\n❌ Usuário não encontrado!");
-            System.out.println("Acesso negado.");
         }
 
+        else{
+            System.out.println("Usuario Não Encontrado!!!");
+        }
+            
         sc.close();
+
+
+
     }
 }
