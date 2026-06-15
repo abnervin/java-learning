a7d3f5d91b70ffd5b380bbaf2d73b3b15def0cc2
VaineO <abnerviniciusa@gmail.com>
2026-06-14 22:40:19 -0400

    Aprimoranddo os quatros dias

--- Day04/While.java
+++ Day04/While.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class While {
     public static void main(String[] args) {
      
-        for(int i = 10; i >= 1 ; i--){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu usuario ");
+        String usr = sc.nextLine();
+
+       
+        if (usr.equals("abner")) {
+
+            System.err.println("Congratularion!!!!");
+            
+
+        }
+
+        else{
+            for(int i = 1000000; i <= 1000000000 ; i--){
             System.out.println(i);
         }
 
+        }
+        
 
     }
 }
