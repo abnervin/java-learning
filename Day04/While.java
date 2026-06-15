8fef91a711f6da15daf07ad35e38f75ceee20544
VaineO <abnerviniciusa@gmail.com>
2026-06-14 22:26:29 -0400

    Aprendendo While

---
+++ Day04/While.java
@@ -0,0 +1,13 @@
+public class While {
+    public static void main(String[] args) {
+     
+        for(int i = 10; i >= 1 ; i--){
+            System.out.println(i);
+        }
+
+
+    }
+}
+
+// estrutura do for é
+//for (acelerador, embreagem, freio);
